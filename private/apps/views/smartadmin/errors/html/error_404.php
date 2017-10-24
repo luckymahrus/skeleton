210c2088193934d5902f3a74b6c3921c45cd0e9c
lucky <lukman.hakim@imaniprima.com>
2017-10-25 02:17:49 +0700

    Adjust Error 404 Page file
    Fixing source URL

--- private/apps/views/smartadmin/errors/html/error_404.php
+++ private/apps/views/smartadmin/errors/html/error_404.php
@@ -29,20 +29,20 @@ include (__DIR__.'/../../layouts/walkernav.php'); ?><!DOCTYPE html>
         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
 
         <!-- Basic Styles -->
-        <link rel="stylesheet" type="text/css" media="screen" href="http://ip-skeleton.dev/assets/themes/smartadmin/css/bootstrap.min.css">
-        <link rel="stylesheet" type="text/css" media="screen" href="http://ip-skeleton.dev/assets/themes/smartadmin/css/font-awesome.min.css">
+        <link rel="stylesheet" type="text/css" media="screen" href="<?=assets_url()?>themes/smartadmin/css/bootstrap.min.css">
+        <link rel="stylesheet" type="text/css" media="screen" href="<?=assets_url()?>themes/smartadmin/css/font-awesome.min.css">
 
         <!-- SmartAdmin Styles : Caution! DO NOT change the order -->
-        <link rel="stylesheet" type="text/css" media="screen" href="http://ip-skeleton.dev/assets/themes/smartadmin/css/smartadmin-production-plugins.min.css">
-        <link rel="stylesheet" type="text/css" media="screen" href="http://ip-skeleton.dev/assets/themes/smartadmin/css/smartadmin-production.min.css">
-        <link rel="stylesheet" type="text/css" media="screen" href="http://ip-skeleton.dev/assets/themes/smartadmin/css/smartadmin-skins.min.css">
+        <link rel="stylesheet" type="text/css" media="screen" href="<?=assets_url()?>themes/smartadmin/css/smartadmin-production-plugins.min.css">
+        <link rel="stylesheet" type="text/css" media="screen" href="<?=assets_url()?>themes/smartadmin/css/smartadmin-production.min.css">
+        <link rel="stylesheet" type="text/css" media="screen" href="<?=assets_url()?>themes/smartadmin/css/smartadmin-skins.min.css">
 
         <!-- SmartAdmin RTL Support -->
-        <link rel="stylesheet" type="text/css" media="screen" href="http://ip-skeleton.dev/assets/themes/smartadmin/css/smartadmin-rtl.min.css"> 
+        <link rel="stylesheet" type="text/css" media="screen" href="<?=assets_url()?>themes/smartadmin/css/smartadmin-rtl.min.css"> 
 
         <!-- We recommend you use "your_style.css" to override SmartAdmin
              specific styles this will also ensure you retrain your customization with each SmartAdmin update. -->
-        <link rel="stylesheet" type="text/css" media="screen" href="http://ip-skeleton.dev/assets/themes/smartadmin/css/style.css">
+        <link rel="stylesheet" type="text/css" media="screen" href="<?=assets_url()?>themes/smartadmin/css/style.css">
 
         <style>
 			.error-text-2
@@ -600,30 +600,30 @@ include (__DIR__.'/../../layouts/walkernav.php'); ?><!DOCTYPE html>
 		    </style>
 
         <!-- Demo purpose only: goes with demo.js, you can delete this css when designing your own WebApp -->
-        <!-- <link rel="stylesheet" type="text/css" media="screen" href="http://ip-skeleton.dev/assets/themes/smartadmin/css/demo.min.css"> -->
+        <!-- <link rel="stylesheet" type="text/css" media="screen" href="<?=assets_url()?>themes/smartadmin/css/demo.min.css"> -->
 
         <!-- FAVICONS -->
-        <link rel="shortcut icon" href="http://ip-skeleton.dev/assets/themes/smartadmin/img/favicon/favicon.ico" type="image/x-icon">
-        <link rel="icon" href="http://ip-skeleton.dev/assets/themes/smartadmin/img/favicon/favicon.ico" type="image/x-icon">
+        <link rel="shortcut icon" href="<?=assets_url()?>themes/smartadmin/img/favicon/favicon.ico" type="image/x-icon">
+        <link rel="icon" href="<?=assets_url()?>themes/smartadmin/img/favicon/favicon.ico" type="image/x-icon">
 
         <!-- GOOGLE FONT -->
         <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Open+Sans:400italic,700italic,300,400,700">
 
         <!-- Specifying a Webpage Icon for Web Clip 
              Ref: https://developer.apple.com/library/ios/documentation/AppleApplications/Reference/SafariWebContent/ConfiguringWebApplications/ConfiguringWebApplications.html -->
-        <link rel="apple-touch-icon" href="http://ip-skeleton.dev/assets/themes/smartadmin/img/splash/sptouch-icon-iphone.png">
-        <link rel="apple-touch-icon" sizes="76x76" href="http://ip-skeleton.dev/assets/themes/smartadmin/img/splash/touch-icon-ipad.png">
-        <link rel="apple-touch-icon" sizes="120x120" href="http://ip-skeleton.dev/assets/themes/smartadmin/img/splash/touch-icon-iphone-retina.png">
-        <link rel="apple-touch-icon" sizes="152x152" href="http://ip-skeleton.dev/assets/themes/smartadmin/img/splash/touch-icon-ipad-retina.png">
+        <link rel="apple-touch-icon" href="<?=assets_url()?>themes/smartadmin/img/splash/sptouch-icon-iphone.png">
+        <link rel="apple-touch-icon" sizes="76x76" href="<?=assets_url()?>themes/smartadmin/img/splash/touch-icon-ipad.png">
+        <link rel="apple-touch-icon" sizes="120x120" href="<?=assets_url()?>themes/smartadmin/img/splash/touch-icon-iphone-retina.png">
+        <link rel="apple-touch-icon" sizes="152x152" href="<?=assets_url()?>themes/smartadmin/img/splash/touch-icon-ipad-retina.png">
         
         <!-- iOS web-app metas : hides Safari UI Components and Changes Status Bar Appearance -->
         <meta name="apple-mobile-web-app-capable" content="yes">
         <meta name="apple-mobile-web-app-status-bar-style" content="black">
         
         <!-- Startup image for web apps -->
-        <link rel="apple-touch-startup-image" href="http://ip-skeleton.dev/assets/themes/smartadmin/img/splash/ipad-landscape.png" media="screen and (min-device-width: 481px) and (max-device-width: 1024px) and (orientation:landscape)">
-        <link rel="apple-touch-startup-image" href="http://ip-skeleton.dev/assets/themes/smartadmin/img/splash/ipad-portrait.png" media="screen and (min-device-width: 481px) and (max-device-width: 1024px) and (orientation:portrait)">
-        <link rel="apple-touch-startup-image" href="http://ip-skeleton.dev/assets/themes/smartadmin/img/splash/iphone.png" media="screen and (max-device-width: 320px)">
+        <link rel="apple-touch-startup-image" href="<?=assets_url()?>themes/smartadmin/img/splash/ipad-landscape.png" media="screen and (min-device-width: 481px) and (max-device-width: 1024px) and (orientation:landscape)">
+        <link rel="apple-touch-startup-image" href="<?=assets_url()?>themes/smartadmin/img/splash/ipad-portrait.png" media="screen and (min-device-width: 481px) and (max-device-width: 1024px) and (orientation:portrait)">
+        <link rel="apple-touch-startup-image" href="<?=assets_url()?>themes/smartadmin/img/splash/iphone.png" media="screen and (max-device-width: 320px)">
 
         
     </head>
@@ -743,61 +743,61 @@ include (__DIR__.'/../../layouts/walkernav.php'); ?><!DOCTYPE html>
         </div>
 
         <!-- PACE LOADER - turn this on if you want ajax loading to show (caution: uses lots of memory on iDevices)-->
-        <script data-pace-options='{ "restartOnRequestAfter": true }' src="http://ip-skeleton.dev/assets/themes/smartadmin/js/plugin/pace/pace.min.js"></script>
+        <script data-pace-options='{ "restartOnRequestAfter": true }' src="<?=assets_url()?>themes/smartadmin/js/plugin/pace/pace.min.js"></script>
 
         <!-- Link to Google CDN's jQuery + jQueryUI; fall back to local -->
         <script src="http://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
         <script>
             if (!window.jQuery) {
-                document.write('<script src="http://ip-skeleton.dev/assets/themes/smartadmin/js/libs/jquery-2.1.1.min.js"><\/script>');
+                document.write('<script src="<?=assets_url()?>themes/smartadmin/js/libs/jquery-2.1.1.min.js"><\/script>');
             }
         </script>
 
         <script src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.10.3/jquery-ui.min.js"></script>
         <script>
             if (!window.jQuery.ui) {
-                document.write('<script src="http://ip-skeleton.dev/assets/themes/smartadmin/js/libs/jquery-ui-1.10.3.min.js"><\/script>');
+                document.write('<script src="<?=assets_url()?>themes/smartadmin/js/libs/jquery-ui-1.10.3.min.js"><\/script>');
             }
         </script>
 
         <!-- IMPORTANT: APP CONFIG -->
-        <script src="http://ip-skeleton.dev/assets/themes/smartadmin/js/app.config.js"></script>
+        <script src="<?=assets_url()?>themes/smartadmin/js/app.config.js"></script>
 
         <!-- JS TOUCH : include this plugin for mobile drag / drop touch events-->
-        <script src="http://ip-skeleton.dev/assets/themes/smartadmin/js/plugin/jquery-touch/jquery.ui.touch-punch.min.js"></script> 
+        <script src="<?=assets_url()?>themes/smartadmin/js/plugin/jquery-touch/jquery.ui.touch-punch.min.js"></script> 
 
         <!-- BOOTSTRAP JS -->
-        <script src="http://ip-skeleton.dev/assets/themes/smartadmin/js/bootstrap/bootstrap.min.js"></script>
+        <script src="<?=assets_url()?>themes/smartadmin/js/bootstrap/bootstrap.min.js"></script>
 
         <!-- CUSTOM NOTIFICATION -->
-        <script src="http://ip-skeleton.dev/assets/themes/smartadmin/js/notification/SmartNotification.min.js"></script>
+        <script src="<?=assets_url()?>themes/smartadmin/js/notification/SmartNotification.min.js"></script>
 
         <!-- JARVIS WIDGETS -->
-        <script src="http://ip-skeleton.dev/assets/themes/smartadmin/js/smartwidgets/jarvis.widget.min.js"></script>
+        <script src="<?=assets_url()?>themes/smartadmin/js/smartwidgets/jarvis.widget.min.js"></script>
 
         <!-- EASY PIE CHARTS -->
-        <script src="http://ip-skeleton.dev/assets/themes/smartadmin/js/plugin/easy-pie-chart/jquery.easy-pie-chart.min.js"></script>
+        <script src="<?=assets_url()?>themes/smartadmin/js/plugin/easy-pie-chart/jquery.easy-pie-chart.min.js"></script>
 
         <!-- SPARKLINES -->
-        <script src="http://ip-skeleton.dev/assets/themes/smartadmin/js/plugin/sparkline/jquery.sparkline.min.js"></script>
+        <script src="<?=assets_url()?>themes/smartadmin/js/plugin/sparkline/jquery.sparkline.min.js"></script>
 
         <!-- JQUERY VALIDATE -->
-        <script src="http://ip-skeleton.dev/assets/themes/smartadmin/js/plugin/jquery-validate/jquery.validate.min.js"></script>
+        <script src="<?=assets_url()?>themes/smartadmin/js/plugin/jquery-validate/jquery.validate.min.js"></script>
 
         <!-- JQUERY MASKED INPUT -->
-        <script src="http://ip-skeleton.dev/assets/themes/smartadmin/js/plugin/masked-input/jquery.maskedinput.min.js"></script>
+        <script src="<?=assets_url()?>themes/smartadmin/js/plugin/masked-input/jquery.maskedinput.min.js"></script>
 
         <!-- JQUERY SELECT2 INPUT -->
-        <script src="http://ip-skeleton.dev/assets/themes/smartadmin/js/plugin/select2/select2.min.js"></script>
+        <script src="<?=assets_url()?>themes/smartadmin/js/plugin/select2/select2.min.js"></script>
 
         <!-- JQUERY UI + Bootstrap Slider -->
-        <script src="http://ip-skeleton.dev/assets/themes/smartadmin/js/plugin/bootstrap-slider/bootstrap-slider.min.js"></script>
+        <script src="<?=assets_url()?>themes/smartadmin/js/plugin/bootstrap-slider/bootstrap-slider.min.js"></script>
 
         <!-- browser msie issue fix -->
-        <script src="http://ip-skeleton.dev/assets/themes/smartadmin/js/plugin/msie-fix/jquery.mb.browser.min.js"></script>
+        <script src="<?=assets_url()?>themes/smartadmin/js/plugin/msie-fix/jquery.mb.browser.min.js"></script>
 
         <!-- FastClick: For mobile devices -->
-        <script src="http://ip-skeleton.dev/assets/themes/smartadmin/js/plugin/fastclick/fastclick.min.js"></script>
+        <script src="<?=assets_url()?>themes/smartadmin/js/plugin/fastclick/fastclick.min.js"></script>
 
         <!--[if IE 8]>
 
@@ -806,15 +806,15 @@ include (__DIR__.'/../../layouts/walkernav.php'); ?><!DOCTYPE html>
         <![endif]-->
 
         <!-- MAIN APP JS FILE -->
-        <script src="http://ip-skeleton.dev/assets/themes/smartadmin/js/app.min.js"></script>
+        <script src="<?=assets_url()?>themes/smartadmin/js/app.min.js"></script>
 
         <!-- ENHANCEMENT PLUGINS : NOT A REQUIREMENT -->
         <!-- Voice command : plugin -->
-        <!-- <script src="http://ip-skeleton.dev/assets/themes/smartadmin/js/speech/voicecommand.min.js"></script> -->
+        <!-- <script src="<?=assets_url()?>themes/smartadmin/js/speech/voicecommand.min.js"></script> -->
 
         <!-- SmartChat UI : plugin -->
-        <!-- <script src="http://ip-skeleton.dev/assets/themes/smartadmin/js/smart-chat-ui/smart.chat.ui.min.js"></script>
-        <script src="http://ip-skeleton.dev/assets/themes/smartadmin/js/smart-chat-ui/smart.chat.manager.min.js"></script> -->
+        <!-- <script src="<?=assets_url()?>themes/smartadmin/js/smart-chat-ui/smart.chat.ui.min.js"></script>
+        <script src="<?=assets_url()?>themes/smartadmin/js/smart-chat-ui/smart.chat.manager.min.js"></script> -->
 
         <!-- PAGE RELATED PLUGIN(S) -->
 
